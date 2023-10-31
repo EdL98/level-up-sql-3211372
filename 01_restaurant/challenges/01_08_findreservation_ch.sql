0f920f81f42569a815684d9dfcdddb56b56fb133
EdL98 <148060423+EdL98@users.noreply.github.com>
2023-10-31 15:55:40 +0000

    Completed 01_08

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,12 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+SELECT 
+ C.FirstName, C.LastName
+FROM
+ Customers AS C
+JOIN 
+ Reservations AS R
+ON
+ C.CustomerID = R.CustomerID
+WHERE LastName LIKE 'St%' AND PartySize =4
