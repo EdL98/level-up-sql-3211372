c4ecc5a017364dd95b22adf1e5298971ca7fab88
EdL98 <148060423+EdL98@users.noreply.github.com>
2023-10-31 16:05:00 +0000

    Completed 01_09

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,12 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+INSERT INTO
+ Customers(FirstName, LastName,email)
+VALUES
+ ('Sam', 'McAdams', 'smac@kinetecoinc.com');
+ 
+INSERT INTO
+ Reservations(Date, PartySize)
+VALUES
+ ('2022-08-12 2022 18:00:00', 5);
